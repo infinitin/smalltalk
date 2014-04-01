55035210847787f081c61af861ea796bcd996434
Nitin <git@nitinbn.com>
2014-04-01 23:15:08 +0800

    Redo: join chat based on strongest wifi signal

--- src/com/example/SmallTalk/Chat.java
+++ src/com/example/SmallTalk/Chat.java
@@ -2,40 +2,38 @@ package com.example.SmallTalk;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
-import org.jivesoftware.smackx.muc.DiscussionHistory;
-import org.jivesoftware.smackx.muc.InvitationListener;
+import org.jivesoftware.smackx.Form;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 import org.jivesoftware.smackx.muc.RoomInfo;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
+import java.util.*;
 
-
-/**
- * Created with IntelliJ IDEA.
- * User: Nitin
- * Date: 11/1/13
- * Time: 5:18 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Chat extends Activity {
-    private Activity activity;
-    protected String signals;
+    WifiManager wifi;
+    ProgressDialog progress;
+    int size = 0;
+    List<ScanResult> results;
+    ArrayList<HashMap<String, Integer>> signals = new ArrayList<HashMap<String, Integer>>();
+    WifiReceiver wifiReceiver = new WifiReceiver();
+
+    private String host = "ejabberd.ro.lt";
+    private int port = 5222;
+    private boolean SASLAuth = true;
+    protected Connection conn;
+    protected String user_id = "";
 
     private Button sendMessageButton;
     private EditText messageText;
@@ -44,127 +42,78 @@ public class Chat extends Activity {
     private ArrayList<String> messageHistory = new ArrayList<String>();
     private ArrayAdapter<String> messageHistoryAdapter;
 
-    private String host = "ejabberd.ro.lt";
-    private int port = 5222;
-    private boolean SASLAuth = true;
-    protected Connection conn;
-    protected String user_id;
-
     private MultiUserChat muc;
     private Connection muc_conn;
-    private String muc_room;
-    //We can allow the user to set all of these in their own settings if we want.
-    private int historyTime = 60;
-    private int historyLength = 10;
+    private String muc_room = "";
 
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat);
 
-        SmackAndroid.init(this);
-
-        Intent intent = getIntent();
-        signals = intent.getStringExtra("JSONSignals");
-        activity = this;
+        progress = new ProgressDialog(this);
+        progress.setTitle("Scanning");
+        progress.show();
+        // To dismiss the dialog
 
         messageHistoryView = (ListView) findViewById(R.id.messageHistoryView);
-        messageHistoryAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, messageHistory);
+        messageHistoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messageHistory);
         messageHistoryView.setAdapter(messageHistoryAdapter);
 
         sendMessageButton = (Button) findViewById(R.id.sendMessageButton);
         messageText = (EditText) findViewById(R.id.message);
         num_viewers = (TextView) findViewById(R.id.num_viewers);
 
-        new XMPPConnect().execute();
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false)
+        {
+            progress.setMessage("Enabling Wifi...");
+            wifi.setWifiEnabled(true);
+        }
+
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        signals.clear();
+        wifi.startScan();
+
+        progress.setMessage("Scanning");
     }
 
-    protected void MUCConnect() {
-        MultiUserChat.addInvitationListener(conn, new InvitationListener() {
-            @Override
-            public void invitationReceived(Connection connection, String s, String s2, String s3, String s4, Message message) {
-                //s is the room, s2 is the inviter, s3 is the reason, s4 is the password
-                muc_conn = connection;
-                muc_room = s;
-                muc = new MultiUserChat(muc_conn, muc_room);
-                DiscussionHistory history = new DiscussionHistory();
-                history.setMaxStanzas(historyLength);
-                history.setSeconds(historyTime);
-                try {
-                    muc.join(user_id, s4, history, SmackConfiguration.getPacketReplyTimeout());
-                    //TODO: Fill screen with the messages found from history
-                } catch (XMPPException e) {
-                    System.err.println("FAILED TO JOIN ROOM");
-                }
-
-                sendMessageButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String message = messageText.getText().toString();
-                        messageText.setText("");
-                        try {
-                            muc.sendMessage(message);
-                        } catch (XMPPException e) {
-                            System.err.println("FAILED TO SEND MESSAGE");
-                        }
-                    }
-                });
-
-                muc.addParticipantListener(new PacketListener() {
-                    @Override
-                    public void processPacket(Packet packet) {
-                        RoomInfo roomInfo = null;
-                        try {
-                            roomInfo = MultiUserChat.getRoomInfo(muc_conn, muc_room);
-                        } catch (XMPPException e) {
-                            System.err.println("FAILED TO GET ROOM INFO");
-                        }
-                        num_viewers = (TextView) findViewById(R.id.num_viewers);
-                        num_viewers.setText(roomInfo != null ? Integer.toString(roomInfo.getOccupantsCount()) : "0");
-                    }
-                });
-
-                muc.addMessageListener(new PacketListener() {
-                    @Override
-                    public void processPacket(Packet packet) {
-                        final Message message = (Message) packet;
-                        messageHistory.add(message.getBody());
-                    }
-                });
-
-            }
-        });
-    }
-
-    private class MUCManagerListener implements ChatManagerListener {
+    class WifiReceiver extends BroadcastReceiver {
         @Override
-        public void chatCreated(org.jivesoftware.smack.Chat chat, boolean b) {
-            chat.addMessageListener(new MessageListener(){
+        public void onReceive(Context c, Intent i)
+        {
+            results = wifi.getScanResults();
 
+            String room = getStrongestResult() + "@conference.ejabberd.ro.lt";
+            if(!muc_room.equals(room)){
+                muc_room = room;
+
+                if(user_id.isEmpty()) {
+                    progress.setMessage("Connecting to server...");
+                    new XMPPConnect().execute();
+                } else {
+                    MUCConnect(3);
+                }
+            }
+        }
+
+        private String getStrongestResult() {
+            Collections.sort(results, new Comparator<ScanResult>() {
                 @Override
-                public void processMessage(org.jivesoftware.smack.Chat chat, Message message) {
-                    messageHistoryView = (ListView) findViewById(R.id.messageHistoryView);
-                    messageHistoryAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, messageHistory);
-                    messageHistoryView.setAdapter(messageHistoryAdapter);
-                    messageHistory.add(message.getBody());
+                public int compare(ScanResult lhs, ScanResult rhs) {
+                    return (lhs.level > rhs.level ? -1 : (lhs.level == rhs.level ? 0 : 1));
                 }
             });
+
+            return results.get(0).BSSID.replaceAll(":", "");
         }
     }
 
-
-    private class XMPPConnect extends AsyncTask<Void, Integer, Connection> {
-        private ProgressDialog progress;
-
-        XMPPConnect() {
-            progress = new ProgressDialog(activity);
-        }
-
-        protected void onPreExecute() {
-            progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progress.setIndeterminate(true);
-            progress.setMessage("Joining epic conversation");
-            progress.show();
-        }
+    class XMPPConnect extends AsyncTask<Void, Integer, Connection> {
 
         protected Connection doInBackground(Void... nothing) {
 
@@ -174,13 +123,12 @@ public class Chat extends Activity {
             try {
                 xmppConn.connect();
             } catch (XMPPException e) {
-                e.printStackTrace();
+                System.err.println("Failed to connect to smalltalk server");
             }
             try {
-                //xmppConn.login("smalltalk", "jabber");
                 xmppConn.loginAnonymously();
             } catch (XMPPException e) {
-                e.printStackTrace();
+                System.err.println("Failed to login to smalltalk server anonymously");
             }
 
             user_id = xmppConn.getUser().substring(0, xmppConn.getUser().indexOf('@'));
@@ -189,72 +137,87 @@ public class Chat extends Activity {
         }
 
         protected void onPostExecute(Connection result) {
-            if (progress.isShowing()) {
-                progress.dismiss();
-            }
-
             conn = result;
-            MUCConnect();
-            new SendWifiDataTask().execute();
+            progress.setMessage("Joining epic conversation");
+            MUCConnect(3);
         }
     }
 
-    class SendWifiDataTask extends AsyncTask<Void, Void, Void> {
-        private ProgressDialog progress;
+    protected void MUCConnect(int tries) {
+        muc_conn = conn;
+        muc = new MultiUserChat(muc_conn, muc_room);
 
-        SendWifiDataTask() {
-            progress = new ProgressDialog(activity);
-        }
-
-        protected void onPreExecute() {
-            progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progress.setIndeterminate(true);
-            progress.setMessage("Getting json signals");
-            progress.show();
-        }
-
-        protected Void doInBackground(Void... x) {
+        try {
+            muc.join(user_id);
+        } catch (XMPPException e) {
             try {
-                JSONArray signals_only = new JSONArray(signals);
-                JSONObject json_signals = new JSONObject();
-                json_signals.put(user_id, signals_only);
-
-                String url = "http://matphillips.com/st/send.php";
-                URL obj = new URL(url);
-                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-                //add reuqest header
-                con.setRequestMethod("POST");
-
-                String urlParameters = "data=" + json_signals.toString();
-
-                // Send post request
-                con.setDoOutput(true);
-                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-                wr.writeBytes(urlParameters);
-                wr.flush();
-                wr.close();
-
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
+                muc.create(user_id);
+                muc.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+            } catch (XMPPException e1) {
+                if(tries > 0) {
+                    MUCConnect(tries - 1);
+                } else {
+                    System.err.println("Failed to join/create room");
                 }
-                in.close();
-
-            } catch (Exception e) {
-                Log.e("HTTP", "Error in http connection " + e.toString());
             }
-
-            return null;
         }
 
-        protected void onPostExecute(Void x) {
-            progress.dismiss();
+        sendMessageButton.setOnClickListener(new SendMessageClick());
+        muc.addParticipantListener(new ParticipantListener());
+        muc.addMessageListener(new MessageListener());
+
+        // This fails when it shouldn't.
+        // We should only need to check for an XMPPException but I can't figure out how to stop it from failing.
+        // So I just catch it for now.
+        RoomInfo roomInfo = null;
+        try {
+            roomInfo = MultiUserChat.getRoomInfo(muc_conn, muc_room);
+        } catch (Exception e) {
+            System.err.println("FAILED TO GET ROOM INFO");
+        }
+        num_viewers = (TextView) findViewById(R.id.num_viewers);
+        num_viewers.setText(roomInfo != null ? Integer.toString(roomInfo.getOccupantsCount()) : "1");
+
+        progress.dismiss();
+    }
+
+    class SendMessageClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String message = messageText.getText().toString();
+            if(!message.isEmpty()) {
+                messageText.setText("");
+                try {
+                    muc.sendMessage(message);
+                } catch (XMPPException e) {
+                    System.err.println("FAILED TO SEND MESSAGE");
+                }
+            }
+        }
+    }
+
+    class ParticipantListener implements PacketListener {
+        @Override
+        public void processPacket(Packet packet) {
+            RoomInfo roomInfo = null;
+            try {
+                roomInfo = MultiUserChat.getRoomInfo(muc_conn, muc_room);
+            } catch (XMPPException e) {
+                System.err.println("FAILED TO GET ROOM INFO");
+            }
+            num_viewers = (TextView) findViewById(R.id.num_viewers);
+            num_viewers.setText(roomInfo != null ? Integer.toString(roomInfo.getOccupantsCount()) : "0");
+        }
+    }
+
+    class MessageListener implements PacketListener {
+        @Override
+        public void processPacket(Packet packet) {
+            final Message message = (Message) packet;
+            messageHistory.add(message.getBody());
+            messageHistoryAdapter.notifyDataSetChanged();
+            messageHistoryView.invalidateViews();
         }
     }
 }
-
--- src/com/example/SmallTalk/RetrieveLocation.java
+++ src/com/example/SmallTalk/RetrieveLocation.java
@@ -8,16 +8,20 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.view.View;
+import android.widget.*;
+import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.RoomInfo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class RetrieveLocation extends Activity {
     WifiManager wifi;
@@ -26,6 +30,23 @@ public class RetrieveLocation extends Activity {
     List<ScanResult> results;
     ArrayList<HashMap<String, Integer>> signals = new ArrayList<HashMap<String, Integer>>();
     WifiReceiver wifiReceiver = new WifiReceiver();
+
+    private String host = "ejabberd.ro.lt";
+    private int port = 5222;
+    private boolean SASLAuth = true;
+    protected Connection conn;
+    protected String user_id;
+
+    private Button sendMessageButton;
+    private EditText messageText;
+    private TextView num_viewers;
+    private ListView messageHistoryView;
+    private ArrayList<String> messageHistory = new ArrayList<String>();
+    private ArrayAdapter<String> messageHistoryAdapter;
+
+    private MultiUserChat muc;
+    private Connection muc_conn;
+    private String muc_room;
 
     /**
      * Called when the activity is first created.
@@ -40,6 +61,14 @@ public class RetrieveLocation extends Activity {
         progress.show();
         progress.getWindow().setGravity(Gravity.BOTTOM);
         // To dismiss the dialog
+
+        messageHistoryView = (ListView) findViewById(R.id.messageHistoryView);
+        messageHistoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messageHistory);
+        messageHistoryView.setAdapter(messageHistoryAdapter);
+
+        sendMessageButton = (Button) findViewById(R.id.sendMessageButton);
+        messageText = (EditText) findViewById(R.id.message);
+        num_viewers = (TextView) findViewById(R.id.num_viewers);
 
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         if (wifi.isWifiEnabled() == false)
@@ -82,33 +111,92 @@ public class RetrieveLocation extends Activity {
                 System.err.println("Shit.");
             }
 
-            progress.setMessage("Joining room...");
+            progress.setMessage("Connecting to server...");
 
-            JSONArray json_signals=new JSONArray();
-            for (Map<String, Integer> signal : signals) {
-                JSONObject json_obj=new JSONObject();
-                for (Map.Entry<String, Integer> entry : signal.entrySet()) {
-                    String ssid = entry.getKey();
-                    Integer strength = entry.getValue();
-                    try {
-                        json_obj.put(ssid,strength);
-                    } catch (JSONException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-                json_signals.put(json_obj);
-            }
-
-            Intent intent = new Intent(RetrieveLocation.this, Chat.class);
-            intent.putExtra("JSONSignals", json_signals.toString());
-            unregisterReceiver(wifiReceiver);
-            startActivity(intent);
-            finish();
+            new XMPPConnect().execute();
 
         }
     }
 
+    class XMPPConnect extends AsyncTask<Void, Integer, Connection> {
 
+        protected Connection doInBackground(Void... nothing) {
 
+            ConnectionConfiguration config = new ConnectionConfiguration(host, port);
+            config.setSASLAuthenticationEnabled(SASLAuth);
+            Connection xmppConn = new XMPPConnection(config);
+            try {
+                xmppConn.connect();
+            } catch (XMPPException e) {
+                System.err.println("Failed to connect to smalltalk server");
+            }
+            try {
+                xmppConn.loginAnonymously();
+            } catch (XMPPException e) {
+                System.err.println("Failed to login to smalltalk server anonymously");
+            }
+
+            user_id = xmppConn.getUser().substring(0, xmppConn.getUser().indexOf('@'));
+
+            return xmppConn;
+        }
+
+        protected void onPostExecute(Connection result) {
+            conn = result;
+            progress.setMessage("Joining epic conversation");
+            MUCConnect();
+        }
+    }
+
+    protected void MUCConnect() {
+        muc_room = signals.get(0).keySet().toArray()[0].toString() + "@conference.ejabberd.ro.lt";
+        muc_conn = conn;
+        muc = new MultiUserChat(muc_conn, muc_room);
+
+        try {
+            muc.join(user_id);
+        } catch (XMPPException e) {
+            System.err.println("Failed to join room");
+        }
+
+        sendMessageButton.setOnClickListener(new SendMessageClick());
+        muc.addParticipantListener(new ParticipantListener());
+        muc.addMessageListener(new MessageListener());
+    }
+
+    class SendMessageClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String message = messageText.getText().toString();
+            messageText.setText("");
+            try {
+                muc.sendMessage(message);
+            } catch (XMPPException e) {
+                System.err.println("FAILED TO SEND MESSAGE");
+            }
+        }
+    }
+
+    class ParticipantListener implements PacketListener {
+        @Override
+        public void processPacket(Packet packet) {
+            RoomInfo roomInfo = null;
+            try {
+                roomInfo = MultiUserChat.getRoomInfo(muc_conn, muc_room);
+            } catch (XMPPException e) {
+                System.err.println("FAILED TO GET ROOM INFO");
+            }
+            num_viewers = (TextView) findViewById(R.id.num_viewers);
+            num_viewers.setText(roomInfo != null ? Integer.toString(roomInfo.getOccupantsCount()) : "0");
+        }
+    }
+
+    class MessageListener implements PacketListener {
+        @Override
+        public void processPacket(Packet packet) {
+            final Message message = (Message) packet;
+            messageHistory.add(message.getBody());
+        }
+    }
 }
